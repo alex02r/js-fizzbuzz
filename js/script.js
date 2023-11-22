a25a901e63e8732a5570a3266c85123354c6db5c
alex02r <alex.giochi02@gmail.com>
2023-11-22 16:10:23 +0100

    [FIX]Adjust messagge output

--- js/script.js
+++ js/script.js
@@ -5,20 +5,25 @@ for(let i=1; i<=100; i++){
     //controlliamo se i è multiplo di 3 e multiplo di 5
     if (i % 3 == 0 && i % 5 == 0) {
         //siamo qui perchè i è multiplo sia di 3 che di 5
-        num = `<li class="text-primary">${i} è multiplo di 3 e di 5</li>`;
+        num = `<li class="text-primary">FizzBuzz</li>`;
         list.innerHTML += num;
 
         console.log(num);
     } else if (i % 3 == 0) {
         //siamo qui perchè i è multiplo di 3 e non di 5
-        num = `<li class="text-secondary">${i} è multiplo di 3</li>`;
+        num = `<li class="text-secondary">Fizz</li>`;
         list.innerHTML += num;
         console.log(num);
     }else if(i % 5 == 0){
         //siamo qui perchè i è multiplo di 5 e non di 3
-        num = `<li class="text-success">${i} è multiplo di 5</li>`;
+        num = `<li class="text-success">Buzz</li>`;
+        list.innerHTML += num;
+        console.log(num);
+    }else{
+        num = `<li class="text-danger">${i} non è divisibile</li>`;
         list.innerHTML += num;
         console.log(num);
     }
+    
     
 }
