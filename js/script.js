525511f42a2ffe1834c6ae5e5fe9afdc10cc4959
alex02r <alex.giochi02@gmail.com>
2023-11-22 15:29:16 +0100

    [UPGRADE]Add ciclo for with control on number

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//iniziamo il ciclo for da 1 a 100
+for(let i=1; i<=100; i++){
+
+    //controlliamo se i è multiplo di 3 e multiplo di 5
+    if (i % 3 == 0 && i % 5 == 0) {
+        //siamo qui perchè i è multiplo sia di 3 che di 5
+
+    } else if (i % 3 == 0) {
+        //siamo qui perchè i è multiplo di 3 e non di 5
+
+    }else if(i % 5 == 0){
+        //siamo qui perchè i è multiplo di 5 e non di 3
+        
+    }
+}
