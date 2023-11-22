28c8e1987d94aed2637f638ab3ede6fcd0c69fc3
alex02r <alex.giochi02@gmail.com>
2023-11-22 16:18:37 +0100

    [FIX]Adjust uotput message

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 //iniziamo il ciclo for da 1 a 100
 for(let i=1; i<=100; i++){
     let num="";
-    const list = document.querySelector('ul'); 
+    const list = document.querySelector('.list'); 
     //controlliamo se i è multiplo di 3 e multiplo di 5
     if (i % 3 == 0 && i % 5 == 0) {
         //siamo qui perchè i è multiplo sia di 3 che di 5
-        num = `<li class="text-primary">FizzBuzz</li>`;
+        num = `FizzBuzz, `;
         list.innerHTML += num;
 
         console.log(num);
     } else if (i % 3 == 0) {
         //siamo qui perchè i è multiplo di 3 e non di 5
-        num = `<li class="text-secondary">Fizz</li>`;
+        num = `Fizz, `;
         list.innerHTML += num;
         console.log(num);
     }else if(i % 5 == 0){
         //siamo qui perchè i è multiplo di 5 e non di 3
-        num = `<li class="text-success">Buzz</li>`;
+        num = `Buzz, `;
         list.innerHTML += num;
         console.log(num);
     }else{
-        num = `<li class="text-danger">${i} non è divisibile</li>`;
+        num = `${i}, `;
         list.innerHTML += num;
         console.log(num);
     }
