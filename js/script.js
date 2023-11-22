99d80f10d3bd95cf8c5c1381b09ce05dfc6c655e
alex02r <alex.giochi02@gmail.com>
2023-11-22 16:00:16 +0100

    [UPGRADE]Add number to list

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
 //iniziamo il ciclo for da 1 a 100
 for(let i=1; i<=100; i++){
     let num="";
+    const list = document.querySelector('ul'); 
     //controlliamo se i è multiplo di 3 e multiplo di 5
     if (i % 3 == 0 && i % 5 == 0) {
         //siamo qui perchè i è multiplo sia di 3 che di 5
-        num = `${i} è multiplo di 3 e di 5`;
+        num = `<li class="text-primary">${i} è multiplo di 3 e di 5</li>`;
+        list.innerHTML += num;
+
         console.log(num);
     } else if (i % 3 == 0) {
         //siamo qui perchè i è multiplo di 3 e non di 5
-        num = `${i} è multiplo di 3`;
+        num = `<li class="text-secondary">${i} è multiplo di 3</li>`;
+        list.innerHTML += num;
         console.log(num);
     }else if(i % 5 == 0){
         //siamo qui perchè i è multiplo di 5 e non di 3
-        num = `${i} è multiplo di 5`;
+        num = `<li class="text-success">${i} è multiplo di 5</li>`;
+        list.innerHTML += num;
         console.log(num);
     }
     
